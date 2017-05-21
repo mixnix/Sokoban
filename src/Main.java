447f32a0cead3d5526e5c0fb0f7c4a12386fe43a
mixnix <nielipinskim@gmail.com>
2017-05-21 11:42:30 +0200

    connected methods so game runs

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
     private static  String IPAdress;
     private static int port;
     public static void main(String[] args){
-
+        Program.run(createSocket());
     }
 
     private static Socket createSocket(){
         try{
             BufferedReader br = new BufferedReader(new FileReader("Config\\server.txt"));
-            IPAdress = br.readLine();
             port = Integer.parseInt(br.readLine());
+            IPAdress = br.readLine();
             Socket serverSocket = new Socket(IPAdress, port);
             OutputStream output = serverSocket.getOutputStream();
             PrintWriter outputWriter = new PrintWriter(output, true);
@@ -31,6 +31,6 @@ public class Main {
             System.out.println("Wystapil blad");
             System.out.println("error: "+e);
         }
-
+        return null;
     }
 }
---
+++ src/Program.java
@@ -0,0 +1,10 @@
+import java.net.Socket;
+
+/**
+ * Created by user_name on 21/05/2017.
+ */
+public class Program {
+    public static void run(Socket socket){
+        SokobanExample.main(new String[0]);
+    }
+}
