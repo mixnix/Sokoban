8ff09a6373eb6b3badef1d893f928eda36687c14
mixnix <nielipinskim@gmail.com>
2017-05-21 12:07:23 +0200

    added code for offline playing and stubs of methods for creating menu and getting settings

--- src/Constants.java
+++ src/Constants.java
@@ -2,4 +2,7 @@
  * Created by user_name on 21/05/2017.
  */
 public class Constants {
+    public static void loadConstants(){
+
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.net.Socket;
 
 /**
@@ -7,8 +8,29 @@ public class Program {
     public static void run(Socket socket){
         if(socket!=null){
             getSettings(socket);
-
+            Constants.loadConstants();
+        } else{
+            System.out.println("Nie udalo sie polaczyc z serwerem");
+            Object[] options={"Tak","Nie"};
+            switch (JOptionPane.showOptionDialog(null,"Nie udalo się połączyć z serwerem, kontynuuować offline?",
+                    "Brak połączenia z serwerem",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[1])){
+                case JOptionPane.YES_OPTION:
+                    break;
+                case JOptionPane.NO_OPTION:
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }
         }
+        utworzMenu(socket);
+    }
+
+    public static void getSettings(Socket socket){
+
+    }
+
+    public static void utworzMenu(Socket socket){
         SokobanExample.main(new String[0]);
     }
 }
