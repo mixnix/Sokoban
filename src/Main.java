d392ef8f47a9416fb8bc82a91af17e8b91781b5b
mixnix <nielipinskim@gmail.com>
2017-05-21 11:32:03 +0200

    method for creating socket to server

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by user_name on 21/05/2017.
  */
 public class Main {
+    private static  String IPAdress;
+    private static int port;
+    public static void main(String[] args){
+
+    }
+
+    private static Socket createSocket(){
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("Config\\server.txt"));
+            IPAdress = br.readLine();
+            port = Integer.parseInt(br.readLine());
+            Socket serverSocket = new Socket(IPAdress, port);
+            OutputStream output = serverSocket.getOutputStream();
+            PrintWriter outputWriter = new PrintWriter(output, true);
+            outputWriter.println("CONNECT");
+            InputStream input = serverSocket.getInputStream();
+            br = new BufferedReader(new InputStreamReader(input));
+            if(br.readLine().contains("CONNECTED")){
+                return serverSocket;
+            } else{
+                return null;
+            }
+        } catch (Exception e){
+            System.out.println("Wystapil blad");
+            System.out.println("error: "+e);
+        }
+
+    }
 }
